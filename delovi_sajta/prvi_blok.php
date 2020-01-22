b6ea2f0d6810ba44748264cc3cc74765b7f82982
Filip Ginder <42473595+FilipGinder@users.noreply.github.com>
2020-01-22 15:09:08 +0100

    Add files via upload

---
+++ delovi_sajta/prvi_blok.php
@@ -0,0 +1,15 @@
+<div class="container">
+  <div class="row">
+        <div class="col-md-12">
+          <h1 id="naslov">Usluge copywriting-a</h1>
+        </div>
+  </div>
+  <div class="row">
+        <div class="col-md-1">
+        </div>
+        <div id="centralni_div" class="col-md-10">
+        </div>
+        <div class="col-md-1">
+        </div>
+  </div>
+</div>
---
+++ delovi_sajta/prvi_blok_profil_korisnika.php
@@ -0,0 +1,14 @@
+<div class="container">
+    <div class="row">
+          <div class="col-md-12">
+            <h1 id="naslov">Usluge copywriting-a</h1>
+            <input type="button" id="nazad" class="btn btn-info" value="Povratak na pocetnu">
+          </div>
+    </div>
+    <div class="row">
+          <div class="col-md-3" id="div_sa_komentarima">
+          </div>
+          <div id="centralni_div_profila" class="col-md-9">
+          </div>
+    </div>
+</div>
